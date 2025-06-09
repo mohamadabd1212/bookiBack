bd6783c467a3629958d4ce5e96ec98af1a0f7175
mohamadabd1212 <mohamad1268ab@gmail.com>
2025-06-09 11:42:25 +0300

    public

--- obj/Debug/net9.0/ruhanBack.AssemblyInfo.cs
+++ obj/Debug/net9.0/ruhanBack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ruhanBack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8af3c6a0c615185e9ef9689cab617bba921c850f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79b574ebef13c58e9dfaec3255d8ede08b352826")]
 [assembly: System.Reflection.AssemblyProductAttribute("ruhanBack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ruhanBack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
